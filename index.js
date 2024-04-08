ff493116795e66294e8f76f93ac2de3028a48df0
Evah <eve-anneoffredi@orange.fr>
2024-04-08 13:30:13 +0200

    👌 IMPROVE: Change db connection location

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const authorization = require("./src/middlewares/authorization.middleware.js");
+
+//Connect to db
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/db.sqlite');
+
 const userRoutes = require("./src/routes/user.js");
 
 app.use(authorization);
