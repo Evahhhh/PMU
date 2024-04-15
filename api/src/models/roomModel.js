556af2d4792929f314bf7143e28e0514d3a2b140
Evah <eve-anneoffredi@orange.fr>
2024-04-15 22:05:36 +0200

    👌 IMPROVE: prettier

--- api/src/models/roomModel.js
+++ api/src/models/roomModel.js
@@ -1,11 +1,10 @@
 class Room {
-    constructor(status, code, adminId, userIds) {
-      this.status = status;
-      this.code = code;
-      this.adminId = adminId;
-      this.userIds = userIds;
-    }
+  constructor(status, code, adminId, userIds) {
+    this.status = status;
+    this.code = code;
+    this.adminId = adminId;
+    this.userIds = userIds;
   }
-  
-  module.exports = Room;
-  
+}
+
+module.exports = Room;
