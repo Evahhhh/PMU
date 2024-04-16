c4065d2d1511fa1047903f3dc0a87f265b012ba1
Evah <eve-anneoffredi@orange.fr>
2024-04-16 23:56:09 +0200

    📦 NEW: round controller

--- api/src/controllers/roundCtrl.js
+++ api/src/controllers/roundCtrl.js
@@ -1,13 +1,107 @@
 const modelRound = require("../models/roundModel");
 
 exports.create = (req, res) => {
+  const { duration, roomId } = req.body;
+  if (duration && roomId) {
+    if (typeof duration !== "number" || typeof roomId !== "number") {
+      return res.status(400).json({
+        error: "duration and roomId must be numbers",
+        errorCode: 5000,
+      });
+    } else {
+      const db = req.db;
+      const newRound = new modelRound(1, duration, roomId);
+      const sqlQuery =
+        "INSERT INTO Round (status, duration, room_id) VALUES (?, ?, ?)";
+      db.run(
+        sqlQuery,
+        [newRound.status, newRound.duration, newRound.roomId],
+        function (err) {
+          if (err) {
+            console.error(err);
+            return res
+              .status(500)
+              .json({ error: "Internal server error", errorCode: 5001 });
+          }
+
+          res.status(200).json({
+            message: "Round created successfully",
+            roundId: this.lastID,
+          });
+        }
+      );
+    }
+  } else {
+    res.status(400).json({ error: "Missing data", errorCode: 5002 });
+  }
 };
 
 exports.disable = (req, res) => {
+  const db = req.db;
+  const roundId = req.params.id;
+
+  const sqlQuery = "UPDATE Round SET status = 0 WHERE round_id = ?";
+  db.run(sqlQuery, roundId, function (err) {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: "Internal server error", errorCode: 5010 });
+    }
+    res.status(200).json({
+      message: "Round disabled successfully",
+      numberRowsUpdated: this.changes,
+    });
+  });
 };
 
 exports.get = (req, res) => {
+  const db = req.db;
+  const roundId = req.params.id;
+
+  const sqlQuery = "SELECT * FROM Round WHERE round_id = ?";
+  db.all(sqlQuery, roundId, (err, results) => {
+    if (results.length === 0) {
+      res.status(400).json({ error: "Invalid data", errorCode: 5020 });
+    } else {
+      res.status(200).json({
+        id: results[0].round_id,
+        status: results[0].status,
+        duration: results[0].duration,
+        roomId: results[0].room_id,
+      });
+    }
+  });
 };
 
 exports.getBets = (req, res) => {
+  const db = req.db;
+  const roundId = req.params.id;
+
+  const sqlQueryRound = "SELECT * FROM Round WHERE round_id = ?";
+  db.get(sqlQueryRound, roundId, (err, round) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: "Internal server error", errorCode: 5030 });
+    }
+
+    if (!round) {
+      return res
+        .status(404)
+        .json({ error: "Round not found", errorCode: 5031 });
+    }
+
+    const sqlQueryBetsRound = "SELECT * FROM Bet WHERE round_id = ?";
+    db.all(sqlQueryBetsRound, roundId, (err, results) => {
+      if (results.length === 0) {
+        res.status(200).json({ msg: "No bets in this round" });
+      } else {
+        res.status(200).json({
+          bets: results,
+        });
+      }
+    });
+  });
 };
