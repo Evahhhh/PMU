9e3f7038ad3b7a457658caefa7fc90e1592f2076
Lisa Genest <lisa.genest02@gmail.com>
2024-04-22 15:01:16 +0200

    Structure front v1

---
+++ web/src/pages/CreationParty.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CreationParty() {
+    return (
+        <h1>Creation de partie</h1>
+    );
+}
+
+export default CreationParty;
---
+++ web/src/pages/Party.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Party() {
+    return (
+        <h1>Party</h1>
+    );
+}
+
+export default Party;
