87e132016c773d50055b15c87fc2c846b47225ca
Evah <eve-anneoffredi@orange.fr>
2024-05-01 16:26:41 +0200

    📦 NEW: user connection & adding notistack for validation/error msg

--- web/src/components/welcome/Login.jsx
+++ web/src/components/welcome/Login.jsx
@@ -1,17 +1,94 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 function Login() {
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      `http://localhost:3000/api/user?email=${email}&password=${password}`
+    );
+    const data = await response.json();
+
+    if (data.errorCode) {
+      switch (data.errorCode) {
+        case 1010:
+          enqueueSnackbar("Aucun compte n'existe avec cet email", {
+            variant: "error",
+          });
+          break;
+        case 1011:
+          enqueueSnackbar("Mot de passe incorrect", {
+            variant: "error",
+          });
+          break;
+        case 1012:
+          enqueueSnackbar("Une erreur est survenue", {
+            variant: "error",
+          });
+          break;
+        case 1013:
+          enqueueSnackbar("Veuillez remplir tous les champs", {
+            variant: "error",
+          });
+          break;
+        case 1014:
+          enqueueSnackbar(
+            "Le mail et le mot de passe doivent être des chaînes de caractère",
+            {
+              variant: "error",
+            }
+          );
+          break;
+        default:
+          enqueueSnackbar("Une erreur inconnue est survenue", {
+            variant: "error",
+          });
+      }
+      return;
+    } else {
+      sessionStorage.setItem("id", data.id);
+      sessionStorage.setItem("token", data.token);
+      enqueueSnackbar("Connecté avec succès", {
+        variant: "success",
+      });
+      navigate("/menu");
+    }
+  };
+
   return (
     <div className="login">
       <h2>CONNEXION</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label className="textField">
           <p className="label">Adresse mail:</p>
-          <input type="email" name="email" className="inputText" />
+          <input
+            type="email"
+            name="email"
+            className="inputText"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="off"
+            required
+          />
         </label>
         <label className="textField">
           <p className="label">Mot de passe:</p>
-          <input type="password" name="password" className="inputText" />
+          <input
+            type="password"
+            name="password"
+            className="inputText"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="off"
+            required
+          />
         </label>
         <input type="submit" value="Se connecter" className="primaryButton" />
       </form>
