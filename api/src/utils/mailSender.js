c244f6482f8521b17f84376f885c098300048249
Evah <eve-anneoffredi@orange.fr>
2024-05-05 20:13:24 +0200

    👌 IMPROVE: mailSender, new mail content with jwt

--- api/src/utils/mailSender.js
+++ api/src/utils/mailSender.js
@@ -1,4 +1,5 @@
 var nodeoutlook = require("nodejs-nodemailer-outlook");
+var jwt = require("jsonwebtoken");
 
 exports.sendEmail = (req, res) => {
   const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
@@ -9,6 +10,13 @@ exports.sendEmail = (req, res) => {
     return res.status(400).send("Invalid email address");
   }
 
+  const token = jwt.sign(
+    { email: emailDestination },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+  const resetLink = `${process.env.PMU_WEBSITE}/reset-password?token=${token}`;
+
   nodeoutlook.sendEmail({
     auth: {
       user: process.env.OUTLOOK_MAIL,
@@ -16,12 +24,33 @@ exports.sendEmail = (req, res) => {
     },
     from: process.env.OUTLOOK_MAIL,
     to: emailDestination,
-    subject: "test",
-    html: "<b>This is bold text</b>",
-    text: "This is text version!",
+    subject: "Réinitialisation de votre mot de passe PMU",
+    html: `
+    <p>Bonjour,</p>
+    <p>Vous avez demandé à réinitialiser votre mot de passe PMU. Veuillez cliquer sur le bouton ci-dessous pour procéder à la réinitialisation.</p><br>
+    <div style="text-align: center;">
+      <a href="${resetLink}" style="background-color: #00A870; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Réinitialiser le mot de passe</a>
+    </div><br><br>
+    <p>Si vous n'avez pas demandé à réinitialiser votre mot de passe, veuillez ignorer cet email.</p>
+    <p>Cordialement,</p>
+    <p>L'équipe PMU</p>
+  `,
+    text: `
+    Bonjour,
+    Vous avez demandé à réinitialiser votre mot de passe PMU. Veuillez cliquer sur le lien ci-dessous pour procéder à la réinitialisation.
+    ${resetLink}
+    Si vous n'avez pas demandé à réinitialiser votre mot de passe, veuillez ignorer cet email.
+    Cordialement,
+    L'équipe PMU
+  `,
     replyTo: "",
 
-    onError: (e) => console.log(e),
-    onSuccess: (i) => res.send("Email envoyé"),
+    onError: (e) => {
+      console.log(e);
+      res.status(500).send({ error: "Error while sending the email", errorCode: 7000});
+    },
+    onSuccess: (i) => {
+      res.status(200).send({ message: "Email sent successfully" });
+    },
   });
 };
