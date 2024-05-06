e22570f3237222af498e804712324c4ea0c9baeb
Evah <eve-anneoffredi@orange.fr>
2024-05-06 18:55:17 +0200

    🐛 FIX: Remove auth in /user for put route

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/userCtrl.js");
-const auth = require("../middlewares/authorization.middleware.js");
 
 router.post("/", userCtrl.signup);
 router.get("/", userCtrl.login);
-router.put("/", auth, userCtrl.modify);
+router.put("/", userCtrl.modify);
 
 module.exports = router;
